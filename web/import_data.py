a0ba840c85a5d68e82194f369cdd654f2b1ff00a
Đinh Việt Cường <dvcuong.work@gmail.com>
2024-08-27 14:08:01 +0700

    Add files via upload

--- web/import_data.py
+++ web/import_data.py
@@ -20,13 +20,21 @@ from werkzeug.routing import BuildError
 import os
 import json
 from app import create_app, db, login_manager, bcrypt
-from models import User, Progress, Test, Universities, QAs, Subject
+from models import User, Progress, Test, Universities, QAs, Subject, SubjectCategory
 
 app = create_app()
 with app.app_context():
+#QAs
+
     folder_path = "data_json/"
     file_names = [f for f in os.listdir(folder_path) if f.endswith('.json')]
     # print(file_names)
+    difficulty_mapping = {
+    'Nhận biết': 0,
+    'Thông hiểu': 1,
+    'Vận dụng': 2,
+    'Vận dụng cao': 3
+    }
     for file_name in file_names:
         file_path = os.path.join(folder_path, file_name)
         if os.path.exists(file_path):
@@ -45,7 +53,7 @@ with app.app_context():
                 if not QAs.query.filter_by(id=item.get('id')).first():
                     new_QAs = QAs(
                         id=item.get('id'),
-                        difficulty=item.get('difficulty'),
+                        difficulty=difficulty_mapping.get(item.get('difficulty'), -1),
                         image=item.get('image'),
                         question=item.get('question'),
                         options=options_str, 
@@ -57,6 +65,7 @@ with app.app_context():
             db.session.commit()
             print(f"Data from {file_name} have been successfully imported into the SQLite database.")
 
+#University Major data
     folder_path = "uni_data/"
     file_names = [f for f in os.listdir(folder_path) if f.endswith('.json')]
     # print(file_names)
@@ -79,6 +88,30 @@ with app.app_context():
                         budget=item.get('học phí'),
                         location=item.get('khu vực'),
                         pass_score=item.get('điểm chuẩn')
+                    )
+                    db.session.add(new_Uni)
+            # Commit the transaction after each file
+            db.session.commit()
+            print(f"Data from {file_name} have been successfully imported into the SQLite database.")
+
+#Subject category data
+    folder_path = "subcat_data/"
+    file_names = [f for f in os.listdir(folder_path) if f.endswith('.json')]
+    # print(file_names)
+    for file_name in file_names:
+        file_path = os.path.join(folder_path, file_name)
+        if os.path.exists(file_path):
+            with open(file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+            
+            # Insert each record into the table
+            for item in data:
+                if not SubjectCategory.query.filter_by(id=item.get('id')).first():
+                    new_Uni = SubjectCategory(
+                        id=item.get('id'),
+                        subject1=item.get('sub1'),
+                        subject2=item.get('sub2'),
+                        subject3=item.get('sub3'),
                     )
                     db.session.add(new_Uni)
             # Commit the transaction after each file
